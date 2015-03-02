e37998367cce576c31b54d9090ec576d273b8307
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-03-02 14:55:58 -0500

    we need this lock

--- block_sampler.h
+++ block_sampler.h
@@ -10,7 +10,7 @@
 #include "array.h"
 
 #define BENCHMARKING
-const uint32_t NUMBER_OF_THREADS = 2;
+const uint32_t NUMBER_OF_THREADS = 1;
 
 class block_description {
 public:
@@ -44,6 +44,7 @@ protected:
   array orignal;
   std::vector<array> downsampled;
   std::mutex mtx_queue;
+  std::mutex mtx_downsampled;
   std::queue<block_description> q;
   uint32_t dim;
 
@@ -73,7 +74,11 @@ private:
         mode = orignal.get_mode_of_block_3d(block.row, block.col, block.depth, block.blocksize, block.blocksize, block.blocksize);
       }
 
-      downsampled[block.downsampled_index][block.downsampled_block_index] = mode;
+      {
+        std::unique_lock<std::mutex> lck(mtx_downsampled);
+        downsampled[block.downsampled_index][block.downsampled_block_index] = mode;
+      }
+
       num_of_blocks--;
     }
   }
