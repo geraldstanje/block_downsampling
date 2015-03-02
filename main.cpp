8f78f7407d3fa6a5977fb6e02bb3e5bc2f63267b
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-03-02 12:10:16 -0500

    added second example

--- main.cpp
+++ main.cpp
@@ -2,17 +2,33 @@
 
 int
 main() {
-  BlockDownSampler2d<8,4> b;
-  uint32_t data[] = {
+  BlockDownSampler2d<8,4> b1;
+  uint32_t data1[] = {
       1,1,1,1,1,1,1,1,
       1,2,1,2,1,2,1,2,
       1,1,2,2,2,2,2,2,
       1,2,2,2,2,2,2,2
   };
-  b.insert(data);
-  b.downsample(4);
-  b.print_original();
-  b.print_downsampled();
+  b1.insert(data1);
+  b1.downsample(4);
+  b1.print_original();
+  b1.print_downsampled();
+
+  BlockDownSampler2d<8,8> b2;
+  uint32_t data2[] = {
+      0,0,0,0,0,0,0,0,
+      1,0,1,0,1,0,1,0,
+      1,1,0,0,1,1,0,0,
+      1,1,1,0,1,1,1,0,
+      0,0,0,0,1,1,1,1,
+      1,0,1,0,1,1,1,1,
+      1,1,1,0,1,1,0,0,
+      1,1,0,0,1,1,0,1
+  };
+  b2.insert(data2);
+  b2.downsample(4);
+  b2.print_original();
+  b2.print_downsampled();
 
   return 0;
 }
